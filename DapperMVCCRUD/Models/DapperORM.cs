f34215899bde129bea4dcd08ddd7ccfd5177b4c7
jibrilz <Kuliah.jibril@gmail.com>
2022-06-23 13:05:25 +0700

    Add project files.

---
+++ DapperMVCCRUD/Models/DapperORM.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperMVCCRUD.Models
+{
+    public class DapperORM
+    {
+        private static string connectionString = @"Data Source=localhost;Initial Catalog=DapperDB;Integrated Security=True;";
+        public static void ExecuteWithoutReturn(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                sqlCon.Execute(procedureName, param, commandType: CommandType.StoredProcedure);
+            }
+
+        }
+
+        //DapperORM.ExecuteReturnScalar<int>(_,_);
+        public static T ExecuteReturnScalar<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                return (T)Convert.ChangeType(sqlCon.ExecuteScalar(procedureName, param, commandType: CommandType.StoredProcedure), typeof(T));
+            }
+
+        }
+        //DapperORM.ReturnList<EmployeeModel> <=  IEnumerable<EmployeeModel>
+        public static IEnumerable<T> ReturnList<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                return sqlCon.Query<T>(procedureName, param, commandType: CommandType.StoredProcedure);
+            }
+
+        }
+    }
+}
---
+++ DapperMVCCRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DapperMVCCRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
